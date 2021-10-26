66009cd7e4ced1df665a4d033ac4f297271f8304
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-10-26 15:56:23 +0200

    Lay out a basic algorithm

--- assistant/lib/assistant/grobid-query-processor.ex
+++ assistant/lib/assistant/grobid-query-processor.ex
@@ -1,5 +1,7 @@
 defmodule Assistant.GrobidQueryProcessor do
 
+  import String
+
   alias Assistant.ZenonService
   alias Assistant.GrobidAdapter
 
@@ -16,25 +18,44 @@ defmodule Assistant.GrobidQueryProcessor do
 
   def query_zenon result do
 
-    author =
-      result[:author]
-      |> String.replace(~r/\sand\s/, " ")
-      |> String.replace(~r/\sund\s/, " ")
-      |> String.replace(~r/[-–,]/, " ")
-      |> String.replace(~r/\s[A-Za-z]\s/, "")
-      |> String.replace(~r/^[A-Za-z]\s/, "")
-      |> String.replace(~r/\s[A-Za-z]$/, "")
-      |> String.split("\s")
-      |> List.first
+    {author, title} = extract_author_and_title result
 
-    title =
-      unless is_nil(result[:title]) do
-        result[:title]
-        |> String.replace(~r["], "")
-        |> String.split(":")
-        |> List.first
+    full_author = case author do
+      {family, given} -> "#{given}.#{family}"
+      _ -> author
+    end
+
+    {_, results} = result = query_zenon_with_author_and_title full_author, title
+    unless Kernel.length(results) == 0 do
+      result
+    else
+      full_author = case author do
+        {family, given} -> family
+        _ -> author
       end
+      {_, results} = result = query_zenon_with_author_and_title full_author, title
+      unless Kernel.length(results) == 0 do
+        result
+      else
+        full_author = case author do
+          {family, given} -> "#{given}.#{family}"
+          _ -> author
+        end
+        {_, results} = result= query_zenon_with_author_and_title full_author, nil
+        unless Kernel.length(results) == 0 do
+          result
+        else
+          full_author = case author do
+            {family, given} -> family
+            _ -> author
+          end
+          query_zenon_with_author_and_title full_author, nil
+        end
+      end
+    end
+  end
 
+  defp query_zenon_with_author_and_title author, title do
     suffix = case {author, title} do
       {author, ""} -> "author:#{author}"
       {author, nil} -> "author:#{author}"
@@ -48,7 +69,40 @@ defmodule Assistant.GrobidQueryProcessor do
     if suffix do
       ZenonService.query_with_suffix suffix
     else
-      ["", []]
+      {"", []}
     end
+  end
+
+  defp extract_author_and_title result do
+    author =
+      result[:author]
+      |> split(~r/\sand\s/) # this is grobid's way of separating authors
+      |> List.first
+
+    num_commas = Kernel.length Regex.scan ~r/,/, author
+
+    author = if num_commas == 1 do
+      author = replace author, "\s", ""
+      [family, given] = split author, ","
+      {family, given}
+    else
+      family = author
+      |> replace(~r/[-–,]/, " ")
+      |> replace(~r/\s[A-Za-z]\s/, "")
+      |> replace(~r/^[A-Za-z]\s/, "")
+      |> replace(~r/\s[A-Za-z]$/, "")
+      |> split("\s")
+      |> List.first
+    end
+
+    title =
+      unless is_nil(result[:title]) do
+        result[:title]
+        |> String.replace(~r["], "")
+        |> String.split(":")
+        |> List.first
+      end
+
+    {author, title}
   end
 end
