06eae5e7f60e7736006bb115389c359ac7c0585c
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-10-28 19:01:50 +0200

    Fix api

--- assistant/lib/assistant_web/controllers/api_controller.ex
+++ assistant/lib/assistant_web/controllers/api_controller.ex
@@ -4,10 +4,9 @@ defmodule AssistantWeb.ApiController do
   alias Plug.Conn
   alias Assistant.Dispatch
 
-  def evaluate conn, _params do
-    {:ok, body, conn} = Conn.read_body conn
-    %{ "parser" => parser, "references" => references } = Poison.decode! body
+  def evaluate conn, %{ "parser" => parser, "references" => references } do
     results = Dispatch.query parser, references
+    results = Enum.map(results, fn [x,y,z] -> %{ given: x, parser_output: y } end)
     json conn, %{ results: results }
   end
 end
