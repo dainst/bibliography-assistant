6c6330aa73b6c25423e10850ef6a90d5f260bc62
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-11-01 18:22:09 +0100

    Improve pattern match

--- assistant/lib/assistant/anystyle_helper.ex
+++ assistant/lib/assistant/anystyle_helper.ex
@@ -2,7 +2,7 @@ defmodule Assistant.AnystyleHelper do
 
   def extract_primary_author item do
     case item["author"] do
-      [%{"family" => family, "given" => given}] -> {family, String.replace(given, ".", "")}
+      [%{"family" => family, "given" => given}|_] -> {family, String.replace(given, ".", "")}
       _ -> nil
     end
   end
