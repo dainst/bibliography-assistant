06948d2a0ece6a30d1af3ae1bbb122c20f0b9a98
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-11-01 20:19:49 +0100

    Improve query processor

--- assistant/lib/assistant/query_processor.ex
+++ assistant/lib/assistant/query_processor.ex
@@ -3,21 +3,40 @@ defmodule Assistant.QueryProcessor do
   import Assistant.ZenonService
 
   def try_queries result, {author_simple, author_complex, title} do
+
+    shortened_title = shorten title
+
     {_, results} = result = query_zenon_with_author_and_title author_complex, title
-    unless Kernel.length(results) == 0 do
+    if length(results) > 0 do
       result
     else
-      {_, results} = result = query_zenon_with_author_and_title author_simple, title
-      unless Kernel.length(results) == 0 do
+      {_, results} = result = query_zenon_with_author_and_title author_complex, shortened_title
+      if length(results) > 0 do
         result
       else
-        {_, results} = result= query_zenon_with_author_and_title author_complex, nil
-        unless Kernel.length(results) == 0 do
+        {_, results} = result = query_zenon_with_author_and_title author_simple, title
+        if length(results) > 0 do
           result
         else
-          query_zenon_with_author_and_title author_simple, nil
+          {_, results} = result = query_zenon_with_author_and_title author_simple, shortened_title
+          if length(results) > 0 do
+            result
+          else
+            {_, results} = result= query_zenon_with_author_and_title author_complex, nil
+            if length(results) > 0 do
+              result
+            else
+              query_zenon_with_author_and_title author_simple, nil
+            end
+          end
         end
       end
     end
+  end
+
+  defp shorten str do
+    str
+    |> String.split(~r/[:.,]/)
+    |> List.first
   end
 end
