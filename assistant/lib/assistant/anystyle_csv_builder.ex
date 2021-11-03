84a4f9a9935f794b2ba3bf78916ca45c2d987cf4
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-11-03 22:54:33 +0100

    Improve csv export

--- assistant/lib/assistant/anystyle_csv_builder.ex
+++ assistant/lib/assistant/anystyle_csv_builder.ex
@@ -4,22 +4,26 @@ defmodule Assistant.AnystyleCsvBuilder do
 
   def generate list do
     entries = Enum.map list, fn [given, entry, _] ->
-      "\"#{extract_primary_author(entry)}\",\"#{List.first(entry["title"])}\",\"#{given}\"\n"
+      "\"#{extract_primary_author(entry) |> escape}\"," <>
+      "\"#{extract_primary_title(entry) |> escape}\"," <>
+      "\"#{given |> escape}\"\n"
     end
-    "\"author\",\"title\",\"given\"\n#{entries}"
+    "\"primary-author\",\"title\",\"given\"\n#{entries}"
   end
 
-  def extract_primary_author nil do
-    ""
+  defp escape entry do
+    String.replace entry, "\"", "\"\""
   end
 
-  def extract_primary_author "" do
-    ""
-  end
+  def extract_primary_title(entry), do: List.first entry["title"]
+
+  def extract_primary_author(nil), do: ""
+
+  def extract_primary_author(""), do: ""
 
   def extract_primary_author entry do
     case AnystyleHelper.extract_primary_author entry do
-      {family, given} -> "#{given}.#{family}"
+      {family, given} -> "#{given}#{family}"
       _ -> ""
     end
   end
