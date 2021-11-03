d4af6434c0b1644e720ef94196d832c379533512
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-11-04 00:46:35 +0100

    Simplify

--- assistant/lib/assistant/query_processor.ex
+++ assistant/lib/assistant/query_processor.ex
@@ -6,18 +6,12 @@ defmodule Assistant.QueryProcessor do
 
     shortened_title = shorten title
 
-    with {_, results} when results == []  <-
-          query_zenon_with_author_and_title(author_complex, title),
-         {_, results} when results == [] <-
-          query_zenon_with_author_and_title(author_complex, shortened_title),
-         {_, results} when results == [] <-
-          query_zenon_with_author_and_title(author_simple, title),
-         {_, results} when results == [] <-
-          query_zenon_with_author_and_title(author_simple, shortened_title),
-         {_, results} when results == [] <-
-          query_zenon_with_author_and_title(author_complex, nil),
-         {_, results} = result when results == [] <-
-          query_zenon_with_author_and_title(author_simple, nil)
+    with {_, []} <- query_zenon_with_author_and_title(author_complex, title),
+         {_, []} <- query_zenon_with_author_and_title(author_complex, shortened_title),
+         {_, []} <- query_zenon_with_author_and_title(author_simple, title),
+         {_, []} <- query_zenon_with_author_and_title(author_simple, shortened_title),
+         {_, []} <- query_zenon_with_author_and_title(author_complex, nil),
+         {_, []} = result <- query_zenon_with_author_and_title(author_simple, nil)
     do
       result
     else
