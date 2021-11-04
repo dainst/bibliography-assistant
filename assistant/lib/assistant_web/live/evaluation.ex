f0c0142f66136a8803aa415b2c73b72ba711652f
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-11-04 21:46:11 +0100

    Work on layout

--- assistant/lib/assistant_web/live/evaluation.ex
+++ assistant/lib/assistant_web/live/evaluation.ex
@@ -11,9 +11,10 @@ defmodule AssistantWeb.Evaluation do
 
   def handle_event "select", %{ "idx" => idx }, socket do
     {idx, _} = Integer.parse idx
+    state = Map.merge(socket.assigns.state, %{ selected_item: idx })
     socket =
       socket
-      |> assign(:selected_item, idx)
+      |> assign(:state, state)
       |> push_event("select", %{ idx: idx })
     {:noreply, socket}
   end
