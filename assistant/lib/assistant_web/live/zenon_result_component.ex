13357d2daab90d862b8584fdd051773b8a6490e4
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-11-12 20:16:24 +0100

    Extract function

--- assistant/lib/assistant_web/live/zenon_result_component.ex
+++ assistant/lib/assistant_web/live/zenon_result_component.ex
@@ -2,4 +2,8 @@ defmodule AssistantWeb.ZenonResultComponent do
   use AssistantWeb, :live_component
 
   alias Assistant.Translator
+
+  def item_url id do
+    "https://zenon.dainst.org/Record/#{id}"
+  end
 end
