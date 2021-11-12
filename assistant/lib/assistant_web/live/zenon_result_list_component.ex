5c92dc7597770c57b4678a6ff5bcbdb7724e6805
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-11-12 20:11:03 +0100

    Extract functions

--- assistant/lib/assistant_web/live/zenon_result_list_component.ex
+++ assistant/lib/assistant_web/live/zenon_result_list_component.ex
@@ -2,4 +2,14 @@ defmodule AssistantWeb.ZenonResultListComponent do
   use AssistantWeb, :live_component
 
   alias AssistantWeb.ZenonResultComponent
+
+  @zenon_url "https://zenon.dainst.org"
+
+  def api_link api_suffix do
+    "#{@zenon_url}/api/v1/search?lookfor=#{api_suffix}"
+  end
+
+  def ui_link ui_suffix do
+    "#{@zenon_url}/Search/Results?join=AND&#{ui_suffix}"
+  end
 end
