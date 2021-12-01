b0c1164d538ce1a08b0041d10208071f3365ffcb
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-12-01 22:03:56 +0100

    Extract helper

--- assistant/lib/assistant/zenon_query_processor.ex
+++ assistant/lib/assistant/zenon_query_processor.ex
@@ -1,17 +1,12 @@
 defmodule Assistant.ZenonQueryProcessor do
 
+  alias Assistant.ZenonQueryHelper
+
   def query_zenon parser_results do
     parser_results
-    |> Enum.map(&prepare_queries_for_parser_result/1)
+    |> Enum.map(&ZenonQueryHelper.prepare_queries_for_parser_result/1)
     |> Enum.reduce_while([], &query_zenon_reducer/2)
     |> Enum.reverse
-  end
-
-  defp prepare_queries_for_parser_result {_parser_result, converted_parser_result} do
-    converted_parser_result
-    |> extract_author_and_title
-    |> convert
-    |> prepare_queries
   end
 
   defp query_zenon_reducer queries_for_parser_result, results do
@@ -35,18 +30,6 @@ defmodule Assistant.ZenonQueryProcessor do
     end
   end
 
-  defp prepare_queries {author_simple, author_complex, title} do
-    shortened_title = shorten title
-    [
-      extract_query_suffixes(author_complex, title),
-      extract_query_suffixes(author_complex, shortened_title),
-      extract_query_suffixes(author_simple, title),
-      extract_query_suffixes(author_simple, shortened_title),
-      extract_query_suffixes(author_complex, ""),
-      extract_query_suffixes(author_simple, "")
-    ]
-  end
-
   defp query_zenon_with_author_and_title suffixes do
 
     IO.inspect elem(suffixes, 0)
@@ -54,31 +37,6 @@ defmodule Assistant.ZenonQueryProcessor do
       query_with_suffix suffixes
     else
       {{"", ""}, {0, []}} # TODO necessary?
-    end
-  end
-
-  defp extract_query_suffixes author, title do
-    if author == "" do
-      case title do
-        nil -> {"", ""}
-        title -> {
-          "title:#{title}",
-          "lookfor0[]=#{title}&type0[]=AllFields"
-        }
-      end
-    else
-      case {author, title} do
-        {author, ""} -> {
-          "author:#{author}",
-          "lookfor0[]=#{author}&type0[]=Author"
-        }
-        {author, title} ->
-          t = String.replace(title," ","+")
-          {
-            "author:#{author} and title:#{title}",
-            "lookfor0[]=#{author}&type0[]=Author&lookfor0[]=#{t}&type0[]=AllFields"
-          }
-      end
     end
   end
 
@@ -104,43 +62,5 @@ defmodule Assistant.ZenonQueryProcessor do
         IO.inspect error
         {:error, :zenon_unreachable}
     end
-  end
-
-  defp shorten nil do
-    ""
-  end
-
-  defp shorten str do
-    str
-    |> String.split(~r/[:.,]/)
-    |> List.first
-  end
-
-  defp convert {author, title} do
-    {simple_name(author) || "", complex_name(author) || "", title || ""}
-  end
-
-  defp extract_author_and_title item do
-    {{item[:"family-name"], item[:"given-name"]}, item[:title]}
-  end
-
-  defp complex_name author do
-    case author do
-      {"", ""} -> ""
-      {family, given} -> "#{remove_dots(given)}.#{family}"
-      _ -> ""
-    end
-  end
-
-  defp simple_name author do
-    case author do
-      {"", _} -> ""
-      {family, _given} -> family
-      _ -> ""
-    end
-  end
-
-  defp remove_dots str do
-    String.replace(str, ".", "")
   end
 end
